84be74a2f773170b5ea021d462c0cab41f29b244
Fabienmcll <Fabienmclll@gmail.com>
2025-06-29 22:37:27 +0200

    - Utilisation de Firebase Authentication pour l'inscription et connexion.

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,24 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import registerRouter from './routes/register.js';
-import { db } from './firebase.js';
+import loginRouter from './routes/login.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: 'http://localhost:8080' }));
+app.use(cors({
+    origin: 'http://localhost:8080',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
 app.use(express.json());
+
 app.use('/register', registerRouter);
-app.use('/login', registerRouter);
+app.use('/', loginRouter);
 
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
